2e8d83337fe0caadc05a005bc67e8f1d7bb27fa8
Samantha Capps <samantha@Cheez.local>
2017-05-02 15:54:56 -0400

    progress

--- ClozeCard.js
+++ ClozeCard.js
@@ -2,17 +2,21 @@ function ClozeCard(text, cloze) {
 	var trigger = false;
 	this.cloze = cloze;
 	this.fullText = text;
-	var splitText = text.split(" ");
-	var splitCloze = cloze.split(" ");
-	for (var i = 0; i < splitCloze.length; i++) {
-		for (var j = 0; j < splitText.length; j++) {
-			if (splitCloze[i] == splitText[j]) {
-				splitText[j] = "_______";
-				trigger = true;
-			}
-		}
+	this.partial = text.replace(cloze, "_______");
+	if (this.partial !== text) {
+		trigger = true;
 	}
-	this.partial = splitText.join(" ");
+	// var splitText = text.split(" ");
+	// var splitCloze = cloze.split(" ");
+	// for (var i = 0; i < splitCloze.length; i++) {
+	// 	for (var j = 0; j < splitText.length; j++) {
+	// 		if (splitCloze[i] == splitText[j]) {
+	// 			splitText[j] = "_______";
+	// 			trigger = true;
+	// 		}
+	// 	}
+	// }
+	// this.partial = splitText.join(" ");
 	if (trigger == false) {
 		console.log("ERROR! CLOZE TEXT IS NOT PART OF FULL TEXT!!");
 		this.cloze = "ERROR!";
@@ -23,7 +27,7 @@ function ClozeCard(text, cloze) {
 
 module.exports = ClozeCard;
 
-var wow = new ClozeCard("My name is Samantha", "Samantha");
+var wow = new ClozeCard("My name is Samantha Capps", "Bob");
 
 console.log(wow.cloze);
 console.log(wow.fullText);
